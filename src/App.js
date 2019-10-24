eb09b4d75e629cc16060609726d91d19c08a92b4
Ariel Rodriguez <ariel_rodriguez@Ariels-Mac-mini.local>
2019-10-23 21:09:36 -0400

    All components are working.  Added a random date picker to update the virtual dom!!!

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ import PhotoDisplayer from "./components/PhotoDisplayer";
 import PhotoTitle from './components/PhotoTitle';
 import PhotoInfo from './components/PhotoInfo';
 
-//const NasaAPIKey = `6VxUxGjWHhbsCJRGcd8nAfY53MmCWoAyDbIPh9hB`
-// const NasaAPIRequest = `https://api.nasa.gov/planetary/apod?api_key=6VxUxGjWHhbsCJRGcd8nAfY53MmCWoAyDbIPh9hB`
+// new Date().toISOString().split('T')[0]
 
 function App() {
   const [nasaData, setNasaData] = useState({})
-  
+  const [date, setDate] = useState(`2019-10-23`)
+
+
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=3wSKKO1Q98tShacOzfRAegVeuBojz8JT5b3B52Qr")
+      .get(`https://api.nasa.gov/planetary/apod?api_key=1LzqvMEPWMIl7ukEc9Ifj3mozXszqGdU791n0LtA`)
       .then(response => {setNasaData(response.data)})
       .catch(error => {(console.log(error))})
   }, [])
 
-  console.log(nasaData)
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=3wSKKO1Q98tShacOzfRAegVeuBojz8JT5b3B52Qr&date=${date}`)
+      .then(response => {setNasaData(response.data)})
+      .catch(error => {(console.log(error))})
+  }, [date])
 
   return (
     <div className="App">
@@ -27,8 +33,7 @@ function App() {
       <PhotoTitle title={nasaData.title}/>
       <PhotoDisplayer imageSrc={nasaData.url}/>
       <PhotoInfo explanation={nasaData.explanation} />
-      <ButtonMaker label="Previous"/>
-      <ButtonMaker label="Next" updateImage={setNasaData} />
+      <ButtonMaker label="Random Date in History" updateImage={setDate} />
     
     </div>
   )
--- src/components/ButtonMaker.js
+++ src/components/ButtonMaker.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
+
+
 export default function ButtonMaker(props) {
-    // console.log(`Button Maker Props: ${props}`)
+
+    function randomDate(start , end) {
+        const datePicker = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+      
+        return datePicker.toISOString().split('T')[0]
+      }
+      
+    let anyDate = randomDate(new Date(2012, 0, 1), new Date());
+        // console.log(`Button Maker Props: ${props}`)
     // why Is the console only laying I'm passing an
     return (
         <div>
-            <button>{props.label}</button>
+            <button onClick={() => props.updateImage(anyDate)}>{props.label}</button>
         </div>
     )
 }
